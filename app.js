5c15cad16a47b4dbb0409b3c1c49228252038f7d
Mark Bishoff <markbish24@gmail.com>
2025-08-13 08:29:38 -0700

    more express

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+const { products } = require("./data");
 
-//setup static and middleware
-app.use(express.static("./public"));
-
-//app.get
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding static assets
-// SSR
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("<h1>Resource Not found</h1>");
+app.get("/", (req, res) => {
+  res.send("<h1> Home Page </h1> <a href='/api/products'> products</a>");
 });
-//app.post
-//app.put
-//app.delete
-//app.all
-//app.use
-//app.listen
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
+});
+app.get("/api/products/:productID", (req, res) => {
+  const { productID } = req.params;
+  const singleProduct = products.find(
+    (product) => product.id === Number(productID)
+  );
+  if (!singleProduct) {
+    return res.status(404).send("Product does not exist");
+  }
+  return res.json(singleProduct);
+});
+
 app.listen(5000, () => {
-  console.log("server is listening on port 5000...");
+  console.log("Server is listening to port 5000...");
 });
