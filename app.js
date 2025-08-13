2373d0a8aac15716d72a42c69186d8bd67eed0d4
Mark Bishoff <markbish24@gmail.com>
2025-08-13 07:27:45 -0700

    using express

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
-//app.get
-app.get("/", (req, res) => {
-  console.log("User entered the resource");
-  res.status(200).send("Home Page");
-});
+//setup static and middleware
+app.use(express.static("./public"));
 
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
-});
+//app.get
+// app.get("/", (req, res) => {
+//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+// adding static assets
+// SSR
+// });
 
 app.all("*", (req, res) => {
   res.status(404).send("<h1>Resource Not found</h1>");
