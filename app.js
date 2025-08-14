7ee479332cf3c90bca73e1f258dc5f93ce480906
Mark Bishoff <markbish24@gmail.com>
2025-08-14 06:51:22 -0700

    more middle ware practice

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const app = express();
+const logger = require("./logger");
+const authorize = require("./authorize");
 
-const logger = (req, res, next) => {
-  const method = req.method;
-  const url = req.url;
-  const time = new Date().getFullYear();
-  console.log(method, url, time);
-  next();
-};
+app.use([logger, authorize]);
 
 app.get("/", logger, (req, res) => {
   res.send("Home");
@@ -15,7 +11,9 @@ app.get("/", logger, (req, res) => {
 app.get("/about", logger, (req, res) => {
   res.send("About");
 });
-
+app.get("./api/products", (req, res) => {
+  res.send("Products");
+});
 app.listen(5000, () => {
   console.log("Server is listening to port 5000...");
 });
