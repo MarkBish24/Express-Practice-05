afd524887f838bbc1ac1277e1774bece3d5582c9
Mark Bishoff <markbish24@gmail.com>
2025-08-14 10:28:37 -0700

    more express practice

--- app.js
+++ app.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const app = express();
-const logger = require("./logger");
-const authorize = require("./authorize");
+let { people } = require("./data");
 
-app.use([logger, authorize]);
+//static assests
+app.use(express.static("./methods-public"));
 
-app.get("/", logger, (req, res) => {
-  res.send("Home");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ success: true, data: people });
 });
-app.get("/about", logger, (req, res) => {
-  res.send("About");
+
+app.post("/login", (req, res) => {
+  const { name } = req.body;
+  if (name) {
+    return res.status(200).send(`Welcome ${name}`);
+  }
+  res.status(401).send("Please Provide Credentials");
 });
-app.get("./api/products", (req, res) => {
-  res.send("Products");
-});
+
 app.listen(5000, () => {
   console.log("Server is listening to port 5000...");
 });
