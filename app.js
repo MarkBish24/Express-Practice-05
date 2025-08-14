b4059161c6374239edd36e7985a8f17879414fc8
Mark Bishoff <markbish24@gmail.com>
2025-08-14 06:09:06 -0700

    more express practice

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.get("/api/products", (req, res) => {
   });
   res.json(newProducts);
 });
+
 app.get("/api/products/:productID", (req, res) => {
   const { productID } = req.params;
   const singleProduct = products.find(
@@ -22,6 +23,23 @@ app.get("/api/products/:productID", (req, res) => {
     return res.status(404).send("Product does not exist");
   }
   return res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+  res.status(200).json(sortedProducts);
+  res.send("hello world");
 });
 
 app.listen(5000, () => {
